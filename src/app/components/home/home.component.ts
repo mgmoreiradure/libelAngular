6cbd3ea24d38035d1a944f012c96b659c2a3b055
mgmoreiradure <Brisa251285>
2018-09-19 14:18:04 -0800

    se agrega a git

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../../services/user.service';
+import {HomeService} from '../../services/home.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private userService: UserService) {
-     this.userService.getPrueba();
+  public data;
+  courseLive: any[]=[];​
+  courseNew: any[]=[];
+  courses: any[]=[];​
+  coursesFree: any[]=[];  ​
+  coursesZbrush: any[]=[];  
+  course1: any[]=[];
+  constructor(private homeService: HomeService,private _router: Router) {
+     
    }
-
+   verCursos(){
+      this._router.navigate(['/cursos']);
+   }
   ngOnInit() {
+    this.homeService.getHome().subscribe((req : any) =>{
+      if(req.courseLive){
+        this.courseLive= req.courseLive;
+      }
+      if(req.courseNew){
+        this.courseNew=req.courseNew;
+      }
+      if(req.courses){
+        this.courses=req.courses;
+      }
+      if(req.coursesFree){
+        this.coursesFree=req.coursesFree;
+      }
+      if(req.coursesZbrush){
+        this.coursesZbrush= req.coursesZbrush;
+        console.log(this.coursesZbrush);
+
+      }
+      if(req.course1){
+        this.course1=req.course1;
+        console.log(this.course1);
+      }
+    });
+    
   }
 
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var  $: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  verRegistro(){
+    $('#myModal').modal('show');
+  }
 }
